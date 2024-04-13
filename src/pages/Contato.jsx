7eea4aa839bb9abc05faa3e09d9b74c710f58a84
Carolaine dos Santos <100321745+carolainesantos@users.noreply.github.com>
2024-04-13 14:40:03 -0300

    add contato

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -1,7 +1,7 @@
 export default function Contato() {
   return (
     <>
-      <section
+      {/* <section
         style={{
           padding: "20px",
           margin: "20px auto",
@@ -10,9 +10,7 @@ export default function Contato() {
           borderRadius: "8px",
           boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
         }}
-      >
-        <h1>Contato ✉️</h1>
-      </section>
+      ></section> */}
 
       <section
         style={{
@@ -24,10 +22,10 @@ export default function Contato() {
           boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
         }}
       >
-        <h1>Fale com a gente</h1>
+        <h1>Fale com a gente 😉</h1>
         <p>Entre em contato conosco</p>
         <p>📞(55) 47 99999-9999</p>
-        <p>✉️ melodify@gmail.com</p>
+        <p>✉️ petamigo@gmail.com</p>
       </section>
     </>
   );
