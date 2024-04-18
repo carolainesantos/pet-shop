cad8d7095f6d8cd51775a7c2b9076bae36006bfb
Carolaine dos Santos <100321745+carolainesantos@users.noreply.github.com>
2024-04-18 20:48:17 -0300

    feate: add carrousel

---
+++ src/components/CarrousselInicial.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { Image } from "react-bootstrap";
+import Carousel from "react-bootstrap/Carousel";
+import "./CarrousselInicial.css";
+
+function CarrosselInicial() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item
+        style={{
+          backgroundImage:
+            "url('https://img.freepik.com/fotos-gratis/adoravel-cachorro-basenji-marrom-e-branco-sorrindo-e-dando-mais-uns-cinco-isolado-no-branco_346278-1657.jpg?random=1')",
+        }}
+        className={"car-item"}
+      >
+        <Carousel.Caption>
+          <h3>Este é Bob</h3>
+          <p>Pastor Alemão</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item
+        style={{
+          backgroundImage:
+            "url('https://www.petz.com.br/blog/wp-content/uploads/2021/04/raca-de-cachorro-docil-2.jpg?random=1')",
+        }}
+        className={"car-item"}
+      >
+        <Carousel.Caption>
+          <h3>Este é Bob</h3>
+          <p>Golden Retriever</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item
+        style={{
+          backgroundImage:
+            "url('https://chefbob.com.br/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2021/05/2021-05-12-como-deixar-os-gatos-mais-tranquilos.jpg.webp?random=1')",
+        }}
+        className={"car-item"}
+      >
+        <Carousel.Caption>
+          <h3>Este é Bob</h3>
+          <p>Golden Retriever</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default CarrosselInicial;
