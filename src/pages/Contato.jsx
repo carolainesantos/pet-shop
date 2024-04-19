87240444c9a32747f11ab8392d388b41b686b9b9
4m4nu4l4 <65728939+4m4nu4l4@users.noreply.github.com>
2024-04-18 21:34:48 -0300

    .

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -3,38 +3,49 @@ import { Col, Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
+const styles = {
+  section: {
+    padding: "20px",
+    margin: "20px auto",
+    maxWidth: "550px",
+    border: "1px solid #4D4D4D",
+    backgroundColor: "#F6F5F5",
+    borderRadius: "8px",
+    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+  },
+  loginSection: {
+    border: "1px solid #4D4D4D",
+    borderRadius: "8px",
+    padding: "20px",
+    margin: "20px 0",
+  },
+};
+
 export default function Contato() {
   return (
-    <>
-      <Container>
-        <Row>
-          <Col md={6}>
-            <section
-              style={{
-                padding: "20px",
-                margin: "20px auto",
-                maxWidth: "550px",
-                backgroundColor: "#F6F5F5",
-                borderRadius: "8px",
-                boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-              }}
-            >
-              <h1>Fale com a gente 😉</h1>
-              <p>
-                Entre em contato conosco <br />
-                08:00 a.m ás 17:00 p.m
-              </p>
+    <Container>
+      <Row>
+        <Col md={6}>
+          <section style={styles.section}>
+            <h1>Venha nos visitar 🐾</h1>
+            <p>
+              Nosso horário de funcionamento
+              <br />
+              Das 08:00 as 17:00
+            </p>
 
-              <p>📞(55) 47 99999-9999</p>
-              <p>✉️ petamigo@gmail.com</p>
-            </section>
-          </Col>
-          <Col md={6}>
+            <p>📞(55) 47 99999-9999</p>
+            <p>✉️ petamigo@gmail.com</p>
+          </section>
+        </Col>
+        <Col md={6}>
+          <section style={styles.loginSection}>
+            <h2>Faça seu Login aqui!</h2>
             <BasicExample />
-          </Col>
-        </Row>
-      </Container>
-    </>
+          </section>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
@@ -43,22 +54,31 @@ function BasicExample() {
     <Container>
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
+          <Form.Label>Email:</Form.Label>
+          <Form.Control type="email" placeholder="Digite seu email" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Label>Senha:</Form.Label>
+          <Form.Control type="password" placeholder="Digite sua senha" />
+          <Form.Text className="text-muted">
+            Não compartilhe sua senha com ninguém!
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
+          <Form.Check type="checkbox" label="Lembrar minha senha" />
         </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
+        <Button
+          style={{
+            backgroundColor: "#C9940B",
+            borderColor: "#C9940B",
+            color: "white",
+            fontWeight: "bold",
+            fontFamily: "Roboto",
+          }}
+          type="submit"
+        >
+          Enviar
         </Button>
       </Form>
     </Container>
